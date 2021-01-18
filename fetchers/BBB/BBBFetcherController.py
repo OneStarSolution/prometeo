790fcd6f9d553d1d8f1469a8f871d0efd0fbaeee
Eduardo Luis Santos Delgado <eduardoluissd@gmail.com>
2021-01-18 13:43:50 -0600

    getting htmls

--- fetchers/BBB/BBBFetcherController.py
+++ fetchers/BBB/BBBFetcherController.py
@@ -1,5 +1,5 @@
-import logging
 import time
+import requests
 from pydantic import HttpUrl
 
 from selenium import webdriver
@@ -10,27 +10,37 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support import expected_conditions as expected_conditions
 
+from fetchers.models.Job import Job
+
 
 class BBBFetcherController:
     WAIT_ELEMENT = 20
     XPATHS = {}
-    BASE_URL = ""
+    BASE_URL = 'https://www.bbb.org/search?find_country={country}&find_loc={location}&find_text={category}'
 
-    def _read_web(self):
+    def _read_web(self, job: Job):
         try:
-            # Delete cookies
-            self.driver.delete_all_cookies()
+            # Change the job catehory if it has spaces
+            job['category'] = job.get('category').replace(' ', "+")
+            target_url = self.BASE_URL.format(**job)
+            print(
+                f"Attempting to crawl BBB using business: {target_url}")
 
-            logging.info(
-                f"Attempting to crawl BBB using business: {self.url_base}")
+            headers = {
+                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.80 Safari/537.36",
+                "referer": target_url,
+            }
 
-            # As BBB don't use to fail just try to connect directly
-            self.driver.get(self.BASE_URL)
+            result = requests.get(target_url, headers=headers)
 
-            # CODE HERE
-
-            return self.driver.page_source
+            return result.text
         except Exception as e:
             print(e)
-        finally:
-            self.driver.close()
+
+
+job = {'country': 'USA', 'location': '96070', 'category': 'Water Treatment'}
+b = BBBFetcherController()
+s = time.perf_counter()
+b._read_web(job)
+e = time.perf_counter()
+print(e-s)
