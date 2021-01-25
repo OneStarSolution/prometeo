e79e733c3ba720980158e76369ee2e79d9e9ec2f
Eduardo Luis Santos Delgado <eduardoluissd@gmail.com>
2021-01-25 14:06:14 -0600

    Get businesses from db

---
+++ scripts/getBusinesses.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+from db.PrometeoDB import PrometeoDB
+
+
+def getBusinesses(collection='yelp', query=None):
+    with PrometeoDB() as db:
+        yelp = db.get_yelp_business()
+        query = {} if not query else query
+        result = [doc for doc in yelp.find(query)]
+        df = pd.DataFrame(result)
+        df.to_csv("YELP.csv")
+
+
+getBusinesses()
