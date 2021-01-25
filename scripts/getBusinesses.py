32b3be2ca15c654560cf9b61dcd632af8488f653
Eduardo Luis Santos Delgado <eduardoluissd@gmail.com>
2021-01-25 14:21:22 -0600

    script get by country

--- scripts/getBusinesses.py
+++ scripts/getBusinesses.py
@@ -6,7 +6,8 @@ from db.PrometeoDB import PrometeoDB
 def getBusinesses(collection='yelp', query=None):
     with PrometeoDB() as db:
         yelp = db.get_yelp_business()
-        query = {} if not query else query
+        query = {"location.country": "US"
+                 } if not query else query
         result = [doc for doc in yelp.find(query)]
         df = pd.DataFrame(result)
         df.to_csv("YELP.csv")
