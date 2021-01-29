093644322b3465c698b49b671c98cb0fde81874f
Eduardo Luis Santos Delgado <eduardoluissd@gmail.com>
2021-01-29 10:57:53 -0600

    scripts

--- scripts/getBusinesses.py
+++ scripts/getBusinesses.py
@@ -5,12 +5,11 @@ from db.PrometeoDB import PrometeoDB
 
 def getBusinesses(collection='yelp', query=None):
     with PrometeoDB() as db:
-        yelp = db.get_yelp_business()
-        query = {"location.country": "CA"
-                 } if not query else query
-        result = [doc for doc in yelp.find(query)]
+        businesses = db.get_businesses()
+        query = {} if not query else query
+        result = [doc for doc in businesses.find(query, {'RAW': 0, 'MD5': 0})]
         df = pd.DataFrame(result)
-        df.to_csv("YELP_CANADA.csv")
+        df.to_csv("BBB_ALL.csv")
 
 
 getBusinesses()
