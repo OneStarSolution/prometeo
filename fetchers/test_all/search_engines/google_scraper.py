d7ef40a27834be1c5095a3c1370f7263f9412303
Vero <vero_quizan@hotmail.com>
2021-02-11 16:00:53 -0800

    separate search engine scrapers

---
+++ fetchers/test_all/search_engines/google_scraper.py
@@ -0,0 +1,37 @@
+def google_scraper(phone_number, pages_per_search_engine):
+    blocked = False
+    google_results = []
+    # pages_per_search_engine = pages_per_search_engine + 1
+    pages_per_search_engine = pages_per_search_engine*10
+    translated_phone = browser_phone_translater(phone_number)
+    search_url = 'https://www.google.com/search?q=' + \
+        translated_phone + '&num=' + str(pages_per_search_engine)
+    driver.get(search_url)
+    html_page = driver.page_source
+    page_soup = soup(html_page, 'html.parser')
+    result_containers = page_soup.findAll("div", {"class": "yuRUbf"})
+    if "Our systems have detected unusual traffic" in page_soup.text:
+        blocked = True
+    for container in result_containers:
+        for link in container.find_all('a'):
+            link = link.get('href')
+            link = str(link)
+            valid_domain = valid_domain_check(link)
+            if valid_domain:
+                valid_url = source_url_filter(link)
+                if valid_url:
+                    if 'https' in link:
+                        link = link.split('https://')[-1]
+                        link = link.split('+')[0]
+                        link = link.split('&')[0]
+                    if '?' in link:
+                        link = link.split('?')[0]
+                    if '%' in link:
+                        pass
+                    else:
+                        if link in google_results:
+                            pass
+                        else:
+                            google_results.append(link)
+                            # print("google ---> " + link)
+    return google_results, blocked
