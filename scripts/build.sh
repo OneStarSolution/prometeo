27ee75893263edcd9f102974a2d870a68108202c
Eduardo Luis Santos Delgado <eduardoluissd@gmail.com>
2021-02-15 19:20:28 -0600

    scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,27 +1,15 @@
-# Script to set up cloud machines
-
-sudo apt update
-
-sudo apt install -y nano git screen unzip zip
-
-# Install nordvpn
-sh <(curl -sSf https://downloads.nordcdn.com/apps/linux/install.sh) -y
-
-# Docker installation and use as sudo
-
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-sudo apt update
-apt-cache policy docker-ce
-sudo apt install -y docker-ce
-# sudo systemctl status docker
-
-sudo usermod -aG docker ${USER}
-su - ${USER}
-id -nG
-
-# Install last version of docker compose
-
-sudo curl -L "https://github.com/docker/compose/releases/download/1.28.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+sudo git clone https://github.com/OneStarSolution/prometeo.git prometeo
+sudo unzip Archive.zip -d prometeo
+sudo mv secrets.env prometeo/config
+sudo mkdir prometeo/data
+sudo mkdir prometeo/data/enhanced
+sudo mkdir prometeo/data/phones_urls
+sudo mkdir prometeo/data/yelp_data
+cd prometeo
+sudo git checkout run_quick
+bash scripts/run.sh
+sudo docker-compose up -d --build
+# screen
+# sudo docker exec -it prometeo_server_1 bash
+# python3 scripts/split_file.py --path valid_zipcodes.csv --chunks 10 --keep 1
+# python3 fetchers/test_all/run.py --workers 2
