4fa68cb3fa67c8b6f8105eff30a5a533258a25ca
Eduardo Luis Santos Delgado <eduardoluissd@gmail.com>
2021-02-24 15:12:27 -0600

    debug manta

--- fetchers/test_all/manta_test.py
+++ fetchers/test_all/manta_test.py
@@ -5,7 +5,11 @@ with open('manta_test_url.txt', 'r') as f:
     urls = f.readlines()
 
 for url in urls[:2]:
-    html = MantaFetcherController(f"https://www.{url}")._read_web()
-    doc = MantaDataScrape(html)
-    doc.parse()
-    print(doc.summary)
+    url = f"https://www.{url}"
+    print(f"url: {url}")
+    html = MantaFetcherController(url)._read_web()
+    if html:
+        print("creating doc")
+        doc = MantaDataScrape(html)
+        doc.parse()
+        print(doc.summary)
