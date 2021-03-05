6beceee402eacd8585ded252c9ae601d45abb503
Eduardo Luis Santos Delgado <eduardoluissd@gmail.com>
2021-03-05 10:12:12 -0600

    basic webservice

---
+++ webservice/app.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+from webservice.routes.enhanced import router as EnhancedRouter
+
+
+app = FastAPI()
+
+app.include_router(EnhancedRouter, tags=["Enhanced"], prefix="/enhanced")
+
+
+@app.get("/", tags=["Root"])
+async def read_root():
+    return {"message": "Welcome to this fantastic app!"}
---
+++ webservice/routes/enhanced.py
@@ -0,0 +1,21 @@
+import os
+import re
+from fastapi import APIRouter
+
+
+router = APIRouter()
+
+
+def count_files(pattern, base_path="data/enhanced"):
+    regex = re.compile(pattern)
+    count = 0
+    for root, dirs, files in os.walk(base_path):
+        for file in files:
+            if regex.match(file):
+                count += 1
+    return count
+
+
+@router.get("/")
+def count(pattern: str):
+    return count_files(pattern)
