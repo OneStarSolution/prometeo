a586ff6108b017d06bdbc1f2b63cefc7c6d25f9c
Eduardo Luis Santos Delgado <eduardoluissd@gmail.com>
2021-03-08 00:19:23 -0600

    named

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,2 +1,2 @@
-screen crawl -q
+screen -S crawl -q
 screen -S crawl -X cd prometeo && sudo git pull && sudo docker-compose up -d --build && sudo docker exec -it prometeo_server_1 bash scripts/run_crawl.sh
