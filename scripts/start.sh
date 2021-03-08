736646b39bad6f43569535e04f1758044382a04e
Eduardo Luis Santos Delgado <eduardoluissd@gmail.com>
2021-03-08 00:24:16 -0600

    no && and deattach

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,3 @@
-screen -S crawl -q
+screen -S crawl -q -d
 cd prometeo
-screen -S crawl -X && sudo git pull && sudo docker-compose up -d --build && sudo docker exec -it prometeo_server_1 bash scripts/run_crawl.sh
+screen -S crawl -X sudo git pull && sudo docker-compose up -d --build && sudo docker exec -it prometeo_server_1 bash scripts/run_crawl.sh
