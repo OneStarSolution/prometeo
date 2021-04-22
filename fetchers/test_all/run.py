8f93ee392b2dc7f096cfc1d15f99e250cbb74221
Eduardo Luis Santos Delgado <eduardoluissd@gmail.com>
2021-04-22 08:57:14 -0500

    garage

--- fetchers/test_all/run.py
+++ fetchers/test_all/run.py
@@ -57,8 +57,7 @@ def get_locations():
 space = "*" * 75
 
 # 'plumbing', 'restoration'
-verticals = ["electrical",
-             "garage door", "restoration", "window cleaning"]
+verticals = ["garage door", "restoration", "window cleaning"]
 
 locations = get_locations()
 
