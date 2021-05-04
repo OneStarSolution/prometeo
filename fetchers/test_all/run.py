0147cf8e662c3afd18fe70f7c758f270356fac23
Eduardo Luis Santos Delgado <eduardoluissd@gmail.com>
2021-05-04 15:02:12 -0500

    red

--- fetchers/test_all/run.py
+++ fetchers/test_all/run.py
@@ -24,25 +24,25 @@ from fetchers.BBB.BBBFetcherController import BBBFetcherController
 
 def get_locations():
 
-    # redistribution = []
-    # if os.path.isfile('redistribution.txt'):
-    #     print("reading redistribution")
-    #     with open("redistribution.txt") as f:
-    #         redistribution = f.readlines()
+    redistribution = []
+    if os.path.isfile('redistribution.txt'):
+        print("reading redistribution")
+        with open("redistribution.txt") as f:
+            redistribution = f.readlines()
 
-    # for zipcode in redistribution:
-    #     yield zipcode
+    for zipcode in redistribution:
+        yield zipcode
 
-    with open('zipcodes_to_crawl.csv', 'r') as f:
-        lines = [line.strip() for line in f.readlines()]
+    # with open('zipcodes_to_crawl.csv', 'r') as f:
+    #     lines = [line.strip() for line in f.readlines()]
 
-    with open('cacities.csv', 'r') as f:
-        canada_lines = [line.replace('\n', '')[:-1]
-                        for line in f.readlines()]
+    # with open('cacities.csv', 'r') as f:
+    #     canada_lines = [line.replace('\n', '')[:-1]
+    #                     for line in f.readlines()]
 
-    for line in lines + canada_lines:
-        # for line in lines:
-        yield line.strip().replace('\n', '').replace('$', '')
+    # for line in lines + canada_lines:
+    #     # for line in lines:
+    #     yield line.strip().replace('\n', '').replace('$', '')
 
 
 space = "*" * 75
